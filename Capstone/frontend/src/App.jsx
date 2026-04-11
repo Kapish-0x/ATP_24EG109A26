ba2fe80b32c53fe3279bfa5169e061d7ab3036ab
Kapish-0x <kapishsinghrawat@gmail.com>
2026-04-11 22:02:43 +0530

    Add index route for Home component

--- Capstone/frontend/src/App.jsx
+++ Capstone/frontend/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       path: '',
       element: <RootLayout />,
       children: [
+        { index: true, element: <Home /> },
         { path: 'home', element: <Home /> },
         { path: 'register', element: <Register /> },
         { path: 'login', element: <Login /> },
