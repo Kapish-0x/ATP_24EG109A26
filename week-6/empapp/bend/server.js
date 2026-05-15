c2c7b8993af783874fe4efc0d1c65f3b3b12978a
Kapish Rawat <kapishsinghrawat@gmail.com>
2026-05-15 14:41:57 +0530

    Modified server

--- week-6/empapp/bend/server.js
+++ week-6/empapp/bend/server.js
@@ -24,10 +24,7 @@ app.use("/api", empApp);
 //connect to db server
 async function connectDB(){
     try{
-        await connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await connect(process.env.MONGODB_URI)
         console.log("db connection successful")
         const port =  process.env.PORT || 4000;
         app.listen(port, ()=>console.log(`server listening to ${port}`))
